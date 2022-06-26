a975265cc2681eac2013391e05994f175f23dfb0
riedayme <programmer.irfaan@gmail.com>
2022-06-26 09:38:56 +0700

    fix bug

--- app.js
+++ app.js
@@ -180,24 +180,6 @@ document.addEventListener("turbo:load", function() {
 		}
 	}
 
-	/* toast */
-	function showToast(message)
-	{
-		$("#main-toast .toast-body").html(message);
-		let bsAlert = new bootstrap.Toast(document.getElementById('main-toast'));
-		bsAlert.show();
-	}
-
-	/* copythis */
-	function copyThis(data) {
-		$("body").append(`<textarea id="gocopy">${data}</textarea>`);
-		$("#gocopy").select();
-		document.execCommand('copy');
-		$("#gocopy").remove();
-		/* show message */
-		showToast('Success Copy');
-	}
-
 	/* related post */
 	function relatedPost()
 	{
@@ -396,3 +378,21 @@ document.addEventListener("turbo:load", function() {
 	}
 
 });	
+
+/* toast */
+function showToast(message)
+{
+	$("#main-toast .toast-body").html(message);
+	let bsAlert = new bootstrap.Toast(document.getElementById('main-toast'));
+	bsAlert.show();
+}
+
+/* copythis */
+function copyThis(data) {
+	$("body").append(`<textarea id="gocopy">${data}</textarea>`);
+	$("#gocopy").select();
+	document.execCommand('copy');
+	$("#gocopy").remove();
+	/* show message */
+	showToast('Success Copy');
+}
